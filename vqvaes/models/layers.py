2be49b63b6fc47e1ca3ffe000a2a3f07c4eaa628
Eric Chen <eric.chen@backflip.ai>
2024-06-30 05:24:38 +0000

    encoder decoder layers

--- vqvaes/models/layers.py
+++ vqvaes/models/layers.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels: int, channels: int):
-        super().__init__(self)
+        super().__init__()
         self.conv = nn.Sequential(
             nn.ReLU(),
             nn.Conv2d(in_channels, channels, kernel_size=3, stride=1, padding=1),
@@ -58,7 +58,7 @@ class Encoder(nn.Module):
             ),
             nn.ReLU(),
             nn.Conv2d(num_channels, num_channels, kernel_size=3, stride=1, padding=1),
-            ResidualStack(in_channels, num_residual_blocks, num_residual_channels),
+            ResidualStack(num_channels, num_residual_blocks, num_residual_channels),
         )
 
     def forward(self, inputs: torch.Tensor):
@@ -66,5 +66,25 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        num_residual_blocks: int,
+        num_residual_channels: int,
+    ):
+        super().__init__()
+        self.decoder = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1),
+            ResidualStack(in_channels, num_residual_blocks, num_residual_channels),
+            nn.ConvTranspose2d(
+                in_channels, in_channels // 2, kernel_size=4, stride=2, padding=1
+            ),
+            nn.ReLU(),
+            nn.ConvTranspose2d(
+                in_channels // 2, out_channels, kernel_size=4, stride=2, padding=1
+            ),
+        )
+
+    def forward(self, x: torch.Tensor):
+        return self.decoder(x)
