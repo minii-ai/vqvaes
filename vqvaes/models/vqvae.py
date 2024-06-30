ee6e2092d1757cc029f5cffeeefc3ed291b49f7c
Eric Chen <eric.chen@backflip.ai>
2024-06-30 17:41:38 +0000

    vqvae implemented

--- vqvaes/models/vqvae.py
+++ vqvaes/models/vqvae.py
@@ -1,11 +1,16 @@
 import torch
 import torch.nn as nn
+from einops import rearrange
 
 from .layers import Decoder, Encoder
 from .vq import VQ
 
 
 class VQVAE(nn.Module):
+    """
+    VQVAE from Neural Discrete Representation Learning "https://arxiv.org/abs/1711.00937"
+    """
+
     def __init__(
         self,
         encoder: Encoder,
@@ -20,11 +25,27 @@ class VQVAE(nn.Module):
         self.vq = vq
         self.decoder = decoder
 
-    def encode(self):
-        pass
+    def encode(self, inputs: torch.Tensor):
+        x = self.encoder(inputs)
+        x = self.pre_vq(x)
 
-    def decode(self):
-        pass
+        # pass encoder output thr. vq layer
+        x = rearrange(x, "b c h w -> b h w c")
+        vq_result = self.vq(x)
+        quantized = rearrange(vq_result["quantized"], "b h w c -> b c h w")
+        result = {
+            **vq_result,
+            "quantized": quantized,
+        }
+
+        return result
+
+    def decode(self, quantized: torch.Tensor):
+        return self.decoder(quantized)
 
     def forward(self, inputs: torch.Tensor):
-        pass
+        vq_result = self.encode(inputs)
+        quantized = vq_result["quantized"]
+        recon = self.decode(quantized)
+
+        return recon, vq_result
